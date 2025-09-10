ecdfe601a8c8b16200d7199045cbfc5d7b4d7968
haenin <gpdnjsqhdld@naver.com>
2025-09-10 11:54:11 +0900

    springboot study

---
+++ springboot-study/chap06-interceptor-lecture-source/src/main/java/com/haenin/chap06/InterceptorTestService.java
@@ -0,0 +1,11 @@
+package com.haenin.chap06;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InterceptorTestService {
+
+    public void test() {
+        System.out.println("Service bean을 활용한 메소드 호출");
+    }
+}

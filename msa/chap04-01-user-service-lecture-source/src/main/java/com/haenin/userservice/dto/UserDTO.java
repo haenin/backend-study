3e1bedb1b389181ccd20001d86453ee5d4d20183
haenin <gpdnjsqhdld@naver.com>
2025-09-25 23:52:07 +0900

    msa study

--- msa/chap04-01-user-service-lecture-source/src/main/java/com/haenin/userservice/dto/UserDTO.java
+++ msa/chap04-01-user-service-lecture-source/src/main/java/com/haenin/userservice/dto/UserDTO.java
@@ -2,6 +2,8 @@ package com.haenin.userservice.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserDTO {
     private String email;
@@ -11,5 +13,8 @@ public class UserDTO {
         고유 회원 아이디 생성하며 추가
          (응답할 때도 활용) */
     private String userId;
+
+    /* 설명. FeignClient 이후(회원이 주문한 내역도 담기) */
+    List<ResponseOrderDTO> orders;
 
 }

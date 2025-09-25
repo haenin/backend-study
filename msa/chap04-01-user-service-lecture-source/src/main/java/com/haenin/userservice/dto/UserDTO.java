290f0ba55d14eea347f216000af329ab8bc9b7f9
haenin <gpdnjsqhdld@naver.com>
2025-09-25 09:27:36 +0900

    commit

---
+++ msa/chap04-01-user-service-lecture-source/src/main/java/com/haenin/userservice/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.haenin.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String email;
+    private String name;
+    private String pwd;
+    /* 설명. ServiceImpl 계층에서
+        고유 회원 아이디 생성하며 추가
+         (응답할 때도 활용) */
+    private String userId;
+
+}

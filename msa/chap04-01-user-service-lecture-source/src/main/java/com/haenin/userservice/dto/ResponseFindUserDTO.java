118be157fb9bed7230e4efceab81c9eaff56e90c
haenin <gpdnjsqhdld@naver.com>
2025-09-25 23:52:02 +0900

    msa study

--- msa/chap04-01-user-service-lecture-source/src/main/java/com/haenin/userservice/dto/ResponseFindUserDTO.java
+++ msa/chap04-01-user-service-lecture-source/src/main/java/com/haenin/userservice/dto/ResponseFindUserDTO.java
@@ -2,10 +2,14 @@ package com.haenin.userservice.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ResponseFindUserDTO {
     private String email;
     private String name;
     private String userId;
 
+    /* 설명. FeignClient 이후(주문내역도 함께) */
+    private List<ResponseOrderDTO> orders;
 }
